65b898d7d29f73a96461e3a626bcd284b07424e5
Liisi <liisi.raidaru@gmail.com>
2013-12-29 20:33:26 +0200

    node modules

---
+++ app.js
@@ -0,0 +1,10 @@
+var app = express();
+
+// Set up port
+app.set('port', process.env.PORT || 5000);
+
+// Set up server
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ package.js
@@ -0,0 +1,20 @@
+{
+	"name": "calendar",
+	"version": "0.0.1",
+	"private": true,
+	"scripts": {
+		"start": "node app.js"
+	},
+	"dependencies": {
+		"express": "3.2.1",
+		"ejs": "*",
+		"mongoose": "*",
+		"async": "*",
+		"superagent": "*",
+		"nodemailer": "0.4"
+	},
+	"devDependencies": {
+		"supertest": "*",
+		"Faker": "*"
+	}
+}
