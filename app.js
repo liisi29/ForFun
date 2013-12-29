9edc39cad4f5ef10ba84b8f83f682df6f5c7254f
Liisi <liisi.raidaru@gmail.com>
2013-12-29 20:51:57 +0200

    set up

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var http = require('http');
+var ejs = require('ejs');
 var app = express();
 
-// Set up port
+// All environments
 app.set('port', process.env.PORT || 5000);
 
-// Set up server
+// Set up views
+app.set('views', __dirname + '/views');
+app.engine('html', ejs.renderFile);
 
+// Set up ejs
+
+ejs.open = '<ejs>';
+ejs.close = '</ejs>';
+
+//Set up middleware
+app.use(express.favicon());	
+app.use(express.json()); 
+app.use(express.urlencoded());
+app.use(app.router); 
+app.use(express.static('public'));
+
+// Reads the routes
+require('./routes/original')(app);
+require('./routes/different')(app);
+
+// Creates server
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
